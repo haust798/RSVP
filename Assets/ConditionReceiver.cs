418050f097ae5d0b34ab2f13faffa5bc10edae82
haust798 <haust798@users.noreply.github.com>
2018-07-30 18:13:34 +1000

    Initial commit

---
+++ Assets/ConditionReceiver.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * BodyDataReceiver.cs
+ *
+ * Receives body data from the network
+ * Requires CustomMessages2.cs
+ */
+
+using HoloToolkit.Sharing;
+using HoloToolkit.Sharing.Tests;
+using HoloToolkit.Unity;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Receives the body data messages
+public class ConditionReceiver : Singleton<ConditionReceiver>
+{
+
+    private Dictionary<long, long> _condition = new Dictionary<long, long>();
+    public static long public_condition;
+
+    public Dictionary<long, long> GetData()
+    {
+        return _condition;
+    }
+
+    void Start()
+    {
+        CustomMessages.Instance.MessageHandlers[CustomMessages.TestMessageID.condition] =
+            this.UpdateCondition;
+    }
+
+    // Called when reading in Kinect body data
+    void UpdateCondition(NetworkInMessage msg)
+    {
+        // Parse the message
+        long surprise = msg.ReadInt64();
+        long trackingID = msg.ReadInt64();
+        public_condition = msg.ReadInt64();
+        //Debug.Log("Condition Messge reieved: "+trackingID+", "+_condition[trackingID]);
+    }
+}
---
+++ Assets/TextController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using TMPro;
+using UnityEngine;
+
+public class TextController : MonoBehaviour
+{
+
+    private string[] book_text;
+    private bool status = false;
+    private int word_index = 0;
+    [SerializeField] string fileName;
+    [SerializeField] float readingSpeed;
+    TMP_Text textMeshDisplay;
+    string[] file_name;
+    private float waitTime = 0;
+    private float timer = 0f;
+    private Stopwatch stopwatch;
+
+    void Start()
+    {
+        stopwatch = new Stopwatch();
+        stopwatch.Start();
+        textMeshDisplay = gameObject.GetComponentInParent<TMP_Text>();
+        file_name = fileName.Split('\\');
+        book_text = ReadString();
+        update_text(file_name[file_name.Length - 1]);
+        waitTime = 60 / readingSpeed*1000;
+        UnityEngine.Debug.Log(waitTime);
+    }
+
+    void Update()
+    {
+        if (word_index == book_text.Length)
+            CancelInvoke();
+        textMeshDisplay.text = book_text[word_index];
+        update_text(textMeshDisplay.text);
+        //timer += stopwatch.ElapsedMilliseconds;
+        //UnityEngine.Debug.Log(timer);
+        UnityEngine.Debug.Log(stopwatch.ElapsedMilliseconds);
+        if (stopwatch.ElapsedMilliseconds > waitTime)
+        {
+            word_index++;
+            //timer = 0f;
+            stopwatch.Reset();
+            stopwatch.Start();
+        }
+    }
+
+    private void update_text(string word)
+    {
+        if (word.IndexOf("\r\n") > 0)
+            word = word.Remove(word.IndexOf("\r\n"), 2);
+        int index = get_colored_letter_index(word);
+        get_richTextBox_location(index);
+        AnimateVertexColors(index);
+    }
+
+    private void get_richTextBox_location(int index)
+    {
+        float X = textMeshDisplay.textInfo.characterInfo[index].topLeft.x;
+        float X_next = textMeshDisplay.textInfo.characterInfo[index].topRight.x;
+        float distanceBefore = textMeshDisplay.textInfo.characterInfo[index].topLeft.x -
+            textMeshDisplay.textInfo.characterInfo[0].topLeft.x;
+        float letter_width = (X_next - X) / 2;
+        GetComponent<RectTransform>().localPosition = new Vector3(0.085f-(distanceBefore+letter_width)*0.005f, 0, -0.01f);
+    }
+
+    private string[] ReadString()
+    {
+        string path = "Assets/Resources/" + fileName + ".txt";
+        StreamReader reader = new StreamReader(path);
+        string textContent = reader.ReadToEnd();
+        reader.Close();
+        textContent = textContent.Replace("\r\n", " ");
+        book_text = textContent.Split(' ');
+        return book_text;
+    }
+
+    private int get_colored_letter_index(string word)
+    {
+        char[] chars_to_trim = { '.', ',', ':', ';', '!', '?', '-' };
+        if (word.Trim(chars_to_trim).Length >= 18)
+            return 5;
+        else if (word.Trim(chars_to_trim).Length >= 14)
+            return 4;
+        else if (word.Trim(chars_to_trim).Length >= 10)
+            return 3;
+        else if (word.Trim(chars_to_trim).Length >= 6)
+            return 2;
+        else if (word.Trim(chars_to_trim).Length >= 2)
+            return 1;
+        else
+            return 0;
+    }
+
+    private void AnimateVertexColors(int index)
+    {
+        TMP_TextInfo textInfo = textMeshDisplay.textInfo;
+        Color32[] newVertexColors;
+        Color32 c0 = textMeshDisplay.color;
+
+        int materialIndex = textInfo.characterInfo[index].materialReferenceIndex;
+        newVertexColors = textInfo.meshInfo[materialIndex].colors32;
+        int vertexIndex = textInfo.characterInfo[index].vertexIndex;
+        if (textInfo.characterInfo[index].isVisible)
+        {
+            c0 = Color.red;
+
+            newVertexColors[vertexIndex + 0] = c0;
+            newVertexColors[vertexIndex + 1] = c0;
+            newVertexColors[vertexIndex + 2] = c0;
+            newVertexColors[vertexIndex + 3] = c0;
+
+            textMeshDisplay.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+        }
+    }
+    
+}
+
